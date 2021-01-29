fecc052ce08e17281f151d3d6e473a38cfeed9e9
Christian Eiselt <dev@eiselt.ch>
2021-01-29 22:21:19 +0100

    Solve day03

--- lib/AOC2020/Common.pm
+++ lib/AOC2020/Common.pm
@@ -10,6 +10,28 @@ sub ltrim { my $s = shift; $s =~ s/^\s+//;       return $s };
 sub rtrim { my $s = shift; $s =~ s/\s+$//;       return $s };
 sub  trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
 
+sub getChars
+{
+  my $self = shift;
+  my $line = shift;
+  my @characters = split(//,$line);
+
+  return \@characters;
+}
+
+sub getCharacterOnPosition
+{
+  my $self = shift;
+  my $fullMap = shift;
+  my $position = shift;
+  my $x = $position->{'x'};
+  my $y = $position->{'y'};
+  my @characterMap = @{$fullMap};
+  my $char = $characterMap[$y][$x];
+
+  return $char;
+}
+
 sub getFileContent
 {
   my $self = shift;
@@ -25,6 +47,15 @@ sub getFileContent
   close(FH);
 
   return \@content;
+}
+
+sub getNumberOfElements
+{
+  my $self = shift;
+  my $list = shift;
+  my $numberOfElements = scalar(@{$list});
+
+  return $numberOfElements;
 }
 
 1;
--- t/day02.t
+++ t/day02.t
@@ -5,20 +5,22 @@ use strict;
 use Test::More tests => 2;
 
 use lib '../lib/';
+use AOC2020::Common;
 use AOC2020::Day02;
 
-my @input = ('1-3 a: abcde','1-3 b: cdefg','2-9 c: ccccccccc');
+my $inputfile = './day02-password-philosophy/test';
+my $input = AOC2020::Common->getFileContent($inputfile);
 
 subtest 'count valid passwords for Part One' => sub
 {
   plan tests => 1;
 
-  is(AOC2020::Day02->getValidPasswordCountForPart(1,\@input),2,'count 2 valid passwords');
+  is(AOC2020::Day02->getValidPasswordCountForPart(1,$input),2,'count 2 valid passwords');
 };
 
 subtest 'count valid passwords for Part Two' => sub
 {
   plan tests => 1;
 
-  is(AOC2020::Day02->getValidPasswordCountForPart(2,\@input),1,'count 1 valid passwords');
+  is(AOC2020::Day02->getValidPasswordCountForPart(2,$input),1,'count 1 valid passwords');
 };
