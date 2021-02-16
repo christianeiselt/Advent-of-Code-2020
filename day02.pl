90a404ae716a218c8706cd08137b735b378b555a
Christian Eiselt <dev@eiselt.ch>
2021-02-16 11:55:19 +0100

    Change subroutine names to lower case and update calls

--- day02.pl
+++ day02.pl
@@ -3,7 +3,7 @@
 use warnings;
 use strict;
 use Carp;
-use version; our $VERSION = qv('1.0.4');
+use version; our $VERSION = qv('1.0.5');
 use lib 'lib';
 use AOC2020::Common;
 use AOC2020::Day02;
@@ -11,8 +11,8 @@ use AOC2020::Day02;
 my $inputfile = './day02_input';
 my $input     = AOC2020::Common->get_file_content($inputfile);
 
-my $SOLUTION_A = AOC2020::Day02->getValidPasswordCountForPart( 1, $input );
-my $SOLUTION_B = AOC2020::Day02->getValidPasswordCountForPart( 2, $input );
+my $SOLUTION_A = AOC2020::Day02->get_valid_password_count_for_part( 1, $input );
+my $SOLUTION_B = AOC2020::Day02->get_valid_password_count_for_part( 2, $input );
 
 print "Solution A: $SOLUTION_A\n" or croak 'no output available';
 print "Solution B: $SOLUTION_B\n" or croak 'no output available';
--- t/day02.t
+++ t/day02.t
@@ -2,7 +2,7 @@
 
 use warnings;
 use strict;
-use version; our $VERSION = qv('1.0.3');
+use version; our $VERSION = qv('1.0.4');
 use Test::More tests => 2;
 
 use lib '../lib/';
@@ -15,13 +15,13 @@ my $input     = AOC2020::Common->get_file_content($inputfile);
 subtest 'count valid passwords for Part One' => sub {
   plan tests => 1;
 
-  is( AOC2020::Day02->getValidPasswordCountForPart( 1, $input ),
+  is( AOC2020::Day02->get_valid_password_count_for_part( 1, $input ),
     2, 'count 2 valid passwords' );
 };
 
 subtest 'count valid passwords for Part Two' => sub {
   plan tests => 1;
 
-  is( AOC2020::Day02->getValidPasswordCountForPart( 2, $input ),
+  is( AOC2020::Day02->get_valid_password_count_for_part( 2, $input ),
     1, 'count 1 valid passwords' );
 };
