93286386d59a6b4c3467c7a3f84a97bf56efaf9f
Christian Eiselt <dev@eiselt.ch>
2021-02-20 22:47:09 +0100

    Comply to PBP Severity 1

--- lib/AOC2020/Common.pm
+++ lib/AOC2020/Common.pm
@@ -6,54 +6,54 @@ use warnings;
 use strict;
 use Carp;
 use English qw( -no_match_vars );
-use version; our $VERSION = qv('1.0.3');
+use version; our $VERSION = qv('1.0.4');
 
 # Based on trim function from Gabor Szabo (www.perlmaven.com)
 sub trim { my $s = shift; $s =~ s/^\s+|\s+$//gmsx; return $s }
 
 sub get_chars {
-  my $self       = shift;
-  my $line       = shift;
-  my @characters = split( //ms, $line );
+    my $self       = shift;
+    my $line       = shift;
+    my @characters = split //ms, $line;
 
-  return \@characters;
+    return \@characters;
 }
 
 sub get_character_on_position {
-  my $self         = shift;
-  my $fullMap      = shift;
-  my $position     = shift;
-  my $x            = $position->{'x'};
-  my $y            = $position->{'y'};
-  my @characterMap = @{$fullMap};
-  my $char         = $characterMap[$y][$x];
+    my $self          = shift;
+    my $full_map      = shift;
+    my $position      = shift;
+    my $x             = $position->{'x'};
+    my $y             = $position->{'y'};
+    my @character_map = @{$full_map};
+    my $char          = $character_map[$y][$x];
 
-  return $char;
+    return $char;
 }
 
 sub get_file_content {
-  my $self     = shift;
-  my $filename = shift;
-  my @content  = ();
+    my $self     = shift;
+    my $filename = shift;
+    my @content  = ();
 
-  open( my $FILE, '<', $filename )
-      or croak "open failed: $OS_ERROR $filename";
+    open my $FILE, '<', $filename
+        or croak "open failed: $OS_ERROR $filename";
 
-  while (<$FILE>) {
-    push( @content, trim($_) );
-  }
+    while (<$FILE>) {
+        push @content, trim($_);
+    }
 
-  close($FILE) or croak "close failed: $CHILD_ERROR, $OS_ERROR $filename";
+    close $FILE or croak "close failed: $CHILD_ERROR, $OS_ERROR $filename";
 
-  return \@content;
+    return \@content;
 }
 
 sub get_number_of_elements {
-  my $self             = shift;
-  my $list             = shift;
-  my $numberOfElements = scalar( @{$list} );
+    my $self               = shift;
+    my $list               = shift;
+    my $number_of_elements = scalar @{$list};
 
-  return $numberOfElements;
+    return $number_of_elements;
 }
 
 1;
