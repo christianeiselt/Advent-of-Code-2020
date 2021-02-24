3de1d198efeb4a3fe9610b14b4151c1856b09e62
Christian Eiselt <dev@eiselt.ch>
2021-02-24 13:47:35 +0100

    Change output

--- day03.pl
+++ day03.pl
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use Carp;
 use Readonly;
-use version; our $VERSION = qv('1.0.6');
+use version; our $VERSION = qv('1.0.7');
 use lib 'lib';
 use AOC2020::Common;
 use AOC2020::Day03;
@@ -12,7 +12,6 @@ use AOC2020::Day03;
 Readonly my $PUZZLE_FILE      => './day03_input';
 Readonly my $PUZZLE_INPUT_REF =>
     AOC2020::Common->get_file_content($PUZZLE_FILE);
-
 Readonly my $SLOPES_REF => {
     'slope_a' => { 'right' => 1, 'down' => 1 },
     'slope_b' => { 'right' => 3, 'down' => 1 },
@@ -20,15 +19,13 @@ Readonly my $SLOPES_REF => {
     'slope_d' => { 'right' => 7, 'down' => 1 },
     'slope_e' => { 'right' => 1, 'down' => 2 },
 };
-my $START_POS_REF = { 'x' => 0, 'y' => 0 };
-
-my $SOLUTION_A =
+Readonly my $START_POS_REF => { 'x' => 0, 'y' => 0 };
+Readonly my $SOLUTION_PART_ONE =>
     AOC2020::Day03->get_tree_count_for_slope( $PUZZLE_INPUT_REF,
     $SLOPES_REF->{'slope_b'},
     $START_POS_REF );
-print "Solution A: $SOLUTION_A\n" or croak 'no output available';
-
-my $SOLUTION_B =
+Readonly my $SOLUTION_PART_TWO =>
     AOC2020::Day03->get_product_of_tree_counts( $PUZZLE_INPUT_REF,
     $SLOPES_REF, $START_POS_REF );
-print "Solution B: $SOLUTION_B\n" or croak 'no output available';
+
+print "Advent of Code 2020 - Day 03 Solutions: Part One: $SOLUTION_PART_ONE / Part Two: $SOLUTION_PART_TWO\n" or croak 'no output available';
