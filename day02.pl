1da11b60d47fe3ea437e34b3a7c58922f4989096
Christian Eiselt <dev@eiselt.ch>
2021-02-24 13:48:38 +0100

    Change output

--- day02.pl
+++ day02.pl
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use Carp;
 use Readonly;
-use version; our $VERSION = qv('1.0.7');
+use version; our $VERSION = qv('1.0.8');
 use lib 'lib';
 use AOC2020::Common;
 use AOC2020::Day02;
@@ -14,12 +14,11 @@ Readonly my $PUZZLE_INPUT_REF =>
     AOC2020::Common->get_file_content($PUZZLE_FILE);
 Readonly my $PART_ONE   => 1;
 Readonly my $PART_TWO   => 2;
-Readonly my $SOLUTION_A =>
+Readonly my $SOLUTION_PART_ONE =>
     AOC2020::Day02->get_valid_password_count_for_part( $PART_ONE,
     $PUZZLE_INPUT_REF );
-Readonly my $SOLUTION_B =>
+Readonly my $SOLUTION_PART_TWO =>
     AOC2020::Day02->get_valid_password_count_for_part( $PART_TWO,
     $PUZZLE_INPUT_REF );
 
-print "Solution A: $SOLUTION_A\n" or croak 'no output available';
-print "Solution B: $SOLUTION_B\n" or croak 'no output available';
+print "Advent of Code 2020 - Day 02 Solutions: Part One: $SOLUTION_PART_ONE / Part Two: $SOLUTION_PART_TWO\n" or croak 'no output available';
