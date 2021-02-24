1aeef0a849f6e93cbe1bbd165610318b0f189661
Christian Eiselt <dev@eiselt.ch>
2021-02-24 13:49:38 +0100

    Change output

--- day01.pl
+++ day01.pl
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use Carp;
 use Readonly;
-use version; our $VERSION = qv('1.0.6');
+use version; our $VERSION = qv('1.0.7');
 use lib 'lib';
 use AOC2020::Common;
 use AOC2020::Day01;
@@ -16,12 +16,11 @@ Readonly my $NUMBER_COUNT_PART_ONE => 2;
 Readonly my $NUMBER_COUNT_PART_TWO => 3;
 Readonly my $TARGET_SUM            => 2020;
 
-my $SOLUTION_A =
+my $SOLUTION_PART_ONE =
     AOC2020::Day01->get_product_of_numbers_adding_to( $TARGET_SUM,
     $PUZZLE_INPUT_REF, $NUMBER_COUNT_PART_ONE );
-my $SOLUTION_B =
+my $SOLUTION_PART_TWO =
     AOC2020::Day01->get_product_of_numbers_adding_to( $TARGET_SUM,
     $PUZZLE_INPUT_REF, $NUMBER_COUNT_PART_TWO );
 
-print "Solution A: $SOLUTION_A\n" or croak 'no output available';
-print "Solution B: $SOLUTION_B\n" or croak 'no output available';
+print "Advent of Code 2020 - Day 01 Solutions: Part One: $SOLUTION_PART_ONE / Part Two: $SOLUTION_PART_TWO\n" or croak 'no output available';
