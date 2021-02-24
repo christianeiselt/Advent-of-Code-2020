3b5bdff370e4054615c7acbbaa82a3b8b268c96c
Christian Eiselt <dev@eiselt.ch>
2021-02-24 13:44:35 +0100

    Change variable names

--- day04.pl
+++ day04.pl
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use Carp;
 use Readonly;
-use version; our $VERSION = qv('1.0.5');
+use version; our $VERSION = qv('1.0.6');
 use lib 'lib';
 use AOC2020::Common;
 use AOC2020::Day04;
@@ -12,7 +12,7 @@ use AOC2020::Day04;
 Readonly my $PUZZLE_FILE      => './day04_input';
 Readonly my $PUZZLE_INPUT_REF =>
     AOC2020::Common->get_file_content($PUZZLE_FILE);
-Readonly my $SOLUTION_A => AOC2020::Day04->get_valid_count_part_one($PUZZLE_INPUT_REF);
-Readonly my $SOLUTION_B => AOC2020::Day04->get_valid_count_part_two($PUZZLE_INPUT_REF);
+Readonly my $SOLUTION_PART_ONE => AOC2020::Day04->get_valid_count_part_one($PUZZLE_INPUT_REF);
+Readonly my $SOLUTION_PART_TWO => AOC2020::Day04->get_valid_count_part_two($PUZZLE_INPUT_REF);
 
-print "Advent of Code 2020 - Day 04 Solutions: Part One: $SOLUTION_A / Part Two: $SOLUTION_B\n" or croak 'no output available';
+print "Advent of Code 2020 - Day 04 Solutions: Part One: $SOLUTION_PART_ONE / Part Two: $SOLUTION_PART_TWO\n" or croak 'no output available';
