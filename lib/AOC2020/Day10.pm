88f03f50f0738065288c2e2ed1d16493010561c6
Christian Eiselt <dev@eiselt.ch>
2021-03-18 21:04:33 +0100

    Refactor Day 10 Part One solution

--- lib/AOC2020/Day10.pm
+++ lib/AOC2020/Day10.pm
@@ -4,9 +4,8 @@ package AOC2020::Day10;
 
 use warnings;
 use strict;
-use List::Util qw(min max);
 use Readonly;
-use version; our $VERSION = qv('1.0.1');
+use version; our $VERSION = qv('1.0.2');
 
 sub solve_part_1 {
     Readonly my $SELF      => shift;
@@ -29,30 +28,83 @@ sub solve {
     return 1;
 }
 
-sub get_list_by_number {
+sub set_occupied {
+    return 1;
+}
+
+sub get_marked_list {
     my $list_ref = shift;
     my @list;
 
     foreach my $number ( @{$list_ref} ) {
-        $list[$number] = 1;
+        $list[$number] = set_occupied();
     }
 
     return \@list;
+}
+
+sub set_zero {
+    return 0;
+}
+
+sub reset_count {
+    Readonly my $INPUT_COUNTER_REF => shift;
+    my $counter_ref = $INPUT_COUNTER_REF;
+
+    $counter_ref->{'count'} = set_zero();
+
+    return $counter_ref;
+}
+
+sub increment_difference_count {
+    Readonly my $INPUT_COUNTER_REF => shift;
+    Readonly my $DIFFERENCE        => shift;
+    my $counter_ref = $INPUT_COUNTER_REF;
+
+    $counter_ref->{$DIFFERENCE}++;
+
+    return $counter_ref;
+}
+
+sub increment_count {
+    Readonly my $INPUT_COUNTER_REF => shift;
+    my $counter_ref = $INPUT_COUNTER_REF;
+
+    $counter_ref->{'count'}++;
+
+    return $counter_ref;
+}
+
+sub set_difference_count {
+    Readonly my $INPUT_COUNTER_REF => shift;
+    Readonly my $DIFFERENCE_1      => 1;
+    Readonly my $DIFFERENCE_3      => 3;
+    Readonly my $COUNT             => $INPUT_COUNTER_REF->{'count'};
+    my $counter_ref = $INPUT_COUNTER_REF;
+
+    if ( $COUNT == $DIFFERENCE_1 ) {
+        $counter_ref = reset_count($counter_ref);
+        $counter_ref =
+            increment_difference_count( $counter_ref, $DIFFERENCE_1 );
+    }
+    elsif ( $COUNT == $DIFFERENCE_3 ) {
+        $counter_ref = reset_count($counter_ref);
+        $counter_ref =
+            increment_difference_count( $counter_ref, $DIFFERENCE_3 );
+    }
+    else {
+        #
+    }
+
+    return $counter_ref;
 }
 
 sub get_counter_for_defined {
     Readonly my $INPUT_COUNTER_REF => shift;
     my $counter_ref = $INPUT_COUNTER_REF;
 
-    $counter_ref->{'count'}++;
-    if ( $counter_ref->{'count'} == 1 ) {
-        $counter_ref->{'count'} = 0;
-        $counter_ref->{1}++;
-    }
-    elsif ( $counter_ref->{'count'} == 3 ) {
-        $counter_ref->{'count'} = 0;
-        $counter_ref->{3}++;
-    }
+    $counter_ref = increment_count($counter_ref);
+    $counter_ref = set_difference_count($counter_ref);
 
     return $counter_ref;
 }
@@ -75,33 +127,41 @@ sub get_counter {
 }
 
 sub add_difference_for_builtin_adapter {
-    Readonly my $COUNTER => shift;
+    Readonly my $INPUT_COUNTER_REF => shift;
+    my $counter_ref = $INPUT_COUNTER_REF;
 
-    return $COUNTER + 1;
+    $counter_ref->{3}++;
+
+    return $counter_ref;
+}
+
+sub prepare_counter {
+    Readonly my $COUNTER_REF => {
+        'count' => -1,
+        1       => 0,
+        3       => 0,
+    };
+
+    return $COUNTER_REF;
 }
 
 sub get_difference_counters {
-    my $list_ref    = shift;
-    my @list        = @{$list_ref};
-    my $counter_ref = {};
-    $counter_ref->{'count'} = -1;
-    $counter_ref->{1}       = 0;
-    $counter_ref->{3}       = 0;
-    my $list_length = scalar @list;
+    Readonly my $LIST_REF    => shift;
+    Readonly my @LIST        => @{$LIST_REF};
+    Readonly my $LIST_LENGTH => scalar @LIST;
+    my $counter_ref = prepare_counter();
+    $counter_ref = add_difference_for_builtin_adapter($counter_ref);
 
-    for my $i ( 0 .. scalar @list ) {
-        $counter_ref = get_counter( $list_ref, $i, $counter_ref );
+    for my $i ( 0 .. $LIST_LENGTH ) {
+        $counter_ref = get_counter( $LIST_REF, $i, $counter_ref );
     }
-
-    $counter_ref->{3} =
-        add_difference_for_builtin_adapter( $counter_ref->{3} );
 
     return $counter_ref;
 }
 
 sub get_differences_product {
     Readonly my $INPUT_REF       => shift;
-    Readonly my $NUMBER_LIST_REF => get_list_by_number($INPUT_REF);
+    Readonly my $NUMBER_LIST_REF => get_marked_list($INPUT_REF);
     Readonly my $COUNTERS_REF    => get_difference_counters($NUMBER_LIST_REF);
 
     return $COUNTERS_REF->{1} * $COUNTERS_REF->{3};
